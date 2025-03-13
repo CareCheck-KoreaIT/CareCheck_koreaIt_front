8beb8449889d7dbc16deb81e1a8d795c477adcca
freemuras <freemuras@gmail.com>
2025-03-13 15:05:53 +0900

    메인레이아웃 수정

---
+++ src/pages/DetailBillPage/DetailBillPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function DetailBillPage() {
+  return (
+    <div>
+        <div>
+            <h2>진료비세부내역서</h2>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default DetailBillPage
