bbf714174e6f7700898693c227c718e9ac0aebc2
freemuras <freemuras@gmail.com>
2025-03-19 17:14:00 +0900

    테이블 분리

---
+++ src/components/TablePageComponents/AdmPatientViatal/style.js
@@ -0,0 +1,20 @@
+import { css } from "@emotion/react";
+
+export const list = css`
+  justify-content: center;
+  width: 100%;
+  border: 0.1rem solid black;
+  border-collapse: collapse;
+  margin: 2rem 0rem 2rem;
+
+  & > tr,
+  td {
+    width: 50rem;
+    height: 5rem;
+    border: 0.1rem solid black;
+    box-sizing: border-box;
+    border: 0.1rem solid #000000;
+    font-size: 2rem;
+    color: #ffffff;
+  }
+`;
