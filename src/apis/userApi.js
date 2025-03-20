3eaa5de55a0af93a8d0e7d1cd7656330b7db425c
photoogu <hanigirl0@gmail.com>
2025-03-20 16:18:08 +0900

    이메일 전화번호 변경 완료

--- src/apis/userApi.js
+++ src/apis/userApi.js
@@ -1,5 +1,7 @@
 import { api } from "../configs/axiosConfig";
 
+export const updateEmailApi = async ({usercode, email}) => await api.put(`/account/users/${usercode}/email`, {email});
+export const updatePhoneNumberApi = async ({usercode, phoneNumber}) => await api.put(`/account/users/${usercode}/phoneNumber`, {phoneNumber});
 export const getUserMeApi = async () => await api.get("/account/users/me");
 
 export const signupApi = async (signupInfo) => {
