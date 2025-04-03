167d799b415dc1ff7a0fc7eff3cd45abcb2d7789
yunggeun0627 <bbiyagi465@gmail.com>
2025-04-03 14:20:42 +0900

    NotFoundPage 생성

--- src/routes/AccountRoute/AccountRoute.jsx
+++ src/routes/AccountRoute/AccountRoute.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AccountPage from '../../pages/AccountPage/AccountPage';
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
 
 function AccountRoute(props) {
     return (
         <>
             <Routes>
                 <Route path='/info' element={<AccountPage />} />
+                <Route path="/*" element={<NotFoundPage />} />
             </Routes>
         </>
     );
